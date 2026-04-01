32680f01daafa6f17c047bb4b5d2660e74d4161c
Hamza Dhibi <hamza.dhibi@tbs.u-tunis.tn>
2026-04-01 09:53:28 +0100

    Week 5: Arrow functions and event handlers

--- src/App.jsx
+++ src/App.jsx
@@ -25,52 +25,50 @@ const stories = [
   },
 ];
 
-function Header() {
-  return <h1>Hacker News</h1>;
-}
+const Header = () => <h1>Hacker News</h1>;
 
-function Search() {
+const List = () => (
+  <div>
+    {stories.map((story) => (
+      <div key={story.objectID}>
+        <h3>
+          <a href={story.url} target="_blank" rel="noreferrer">
+            {story.title}
+          </a>
+        </h3>
+        <p>Author: <span>{story.author}</span></p>
+        <p>Points: <span>{story.points}</span></p>
+        <p>Comments: <span>{story.num_comments}</span></p>
+      </div>
+    ))}
+  </div>
+);
+
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    console.log("User is typing...");
+  };
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <input type="text" id="search" onChange={handleChange} />
     </div>
   );
-}
+};
 
-function List() {
-  return (
-    <div>
-      {stories.map((story) => (
-        <div key={story.objectID}>
-          <h3>
-            <a href={story.url} target="_blank" rel="noreferrer">
-              {story.title}
-            </a>
-          </h3>
-          <p>Author: <span>{story.author}</span></p>
-          <p>Points: <span>{story.points}</span></p>
-          <p>Comments: <span>{story.num_comments}</span></p>
-        </div>
-      ))}
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Header />
+    <Search />
+    <List />
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Search />
-      <List />
-
-      {/* Reflection */}
-      {/* 1. App is the root component that assembles everything */}
-      {/* 2. List is responsible for rendering the stories */}
-      {/* 3. Search is responsible for the search input UI */}
-      {/* 4. This structure is cleaner because each component has one job */}
-    </div>
-  );
-}
+    {/* Reflection */}
+    {/* 1. Concise body arrow functions are used when only returning a value */}
+    {/* 2. Block body arrow functions are used when we need logic inside */}
+    {/* 3. Event object contains info about the event like target, value, type */}
+  </div>
+);
 
 export default App;
